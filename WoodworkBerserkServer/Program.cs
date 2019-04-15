007c93e93daf157b270eeb7717316ff03aaf3ed6
petertbdtu <s175210@student.dtu.dk>
2019-04-15 10:45:09 +0200

    Add project files.

---
+++ WoodworkBerserkServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WoodworkBerserkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
